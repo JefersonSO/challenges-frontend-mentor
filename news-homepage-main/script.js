1719e85deb328dbb7a708cbcbcf65598694dedd8
JefersonSO <115329121+JefersonSO@users.noreply.github.com>
2023-03-30 20:05:07 -0300

    fim projeto News Homepage

---
+++ news-homepage-main/script.js
@@ -0,0 +1,17 @@
+
+
+
+abrirMenu.addEventListener('click', clicou1x)
+fecharMenu.addEventListener('click', clicou2x)
+
+
+function clicou1x(){
+    menu.style.display = 'flex'
+}
+
+function clicou2x(){
+    
+    menu.style.display = 'none'
+    
+}
+ 
